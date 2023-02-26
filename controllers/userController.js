16bcff44e42b2cc62eac2e199f7e59582c9c42cf
Emil1577 <emilronquillo@gmail.com>
2023-02-26 00:16:56 -0800

    pushing new files

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ module.exports = {
   // Get all users
   getUsers(req, res) {
     User.find({})
-      .populate({ path: 'thoughts', select: '-__v' })
+      .populate({ path: 'thoughts', select: '-__v' }) //'thoughts' is from the user.thoughts this goes the same with friends below.
       .populate({ path: 'friends', select: '-__v' })
       .then(dbUserData => res.json(dbUserData))
       .catch(err => {
