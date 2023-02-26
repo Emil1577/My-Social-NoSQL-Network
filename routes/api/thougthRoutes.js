46fa845e41e4dda50ddfb6a3dfd07532b14353a3
Emil1577 <emilronquillo@gmail.com>
2023-02-25 17:36:44 -0800

    settingup controllers

---
+++ routes/api/thougthRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addTag,
+  removeTag,
+} = require('../../controllers/appController');
+
+// /api/Thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/Thoughts/:ThoughtId
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/Thoughts/:ThoughtId/tags
+router.route('/:thoughtId/tags').post(addTag);
+
+// /api/Thoughts/:ThoughtId/tags/:tagId
+router.route('/:thoughtId/tags/:tagId').delete(removeTag);
+
+module.exports = router;
